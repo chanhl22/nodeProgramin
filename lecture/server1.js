f836b6523822a02822eee844e894d9ac8513421e
chanhl22 <chanhlee@student.42seoul.kr>
2021-02-09 19:52:22 +0900

    server update

--- lecture/server1.js
+++ lecture/server1.js
@@ -1,4 +1,4 @@
-const http = require('http'); //노드가 http 쉽게 만들 수 있게 제공
+/*const http = require('http'); //노드가 http 쉽게 만들 수 있게 제공
 
 //서버를 만들자
 http.createServer((req,res) => {
@@ -9,4 +9,24 @@ http.createServer((req,res) => {
 
     .listen(8080, () => {
         console.log('8080번 포트에서 서버 대기 중입니다.');
-    });
+    });*/
+
+
+//에러처리
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+    res.write('<h1>Hello Node!</h1>');
+    res.write('<P>Hello server</p>');
+    res.end('<p>Hello chan</p>');
+})
+
+    .listen(8080);
+    
+server.on('listening',error => {
+        console.log('8080번 포트에서 서버 대기 중입니다.');
+})
+
+server.on('error',error => {
+    console.error(error);
+})
